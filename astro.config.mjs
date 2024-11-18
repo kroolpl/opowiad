d483cfe933129ef23bfcfa7d5c24d7b8a286c930
qrol <ggglondyn@gmail.com>
2024-11-18 10:46:10 +0100

    Startv4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify({
-    edgeMiddleware: true
+    dist: new URL('./dist/', import.meta.url),
+    functionPerRoute: false
   }),
   vite: {
     envDir: '.',
